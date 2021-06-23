cd273bfb45a8ee1447ebd80fd19381b1638849e0
Cory Romdenne <cromdenne@aleysian.com>
2021-06-23 10:21:54 -0500

    Add URL parameter extraction and redirection

---
+++ js/main.js
@@ -0,0 +1,31 @@
+function redirect() {
+    // Get the URL of the current page (the callback URL) and split at parameters
+    let url = window.location.href.split('&');
+    
+    // Variables to pass back to the origination site (Metadata Tracker)
+    let authCode = '';
+    let origURL  = '';
+
+    // Traverse the callback URL parameters
+    url.forEach(element => {
+        
+        // Extract the authorization code
+        if (element.startsWith('code=')) {
+            authCode = element.substring(5, element.length).replaceAll('%3D', '');
+        }
+
+        // Extract the origination URL from the state parameter
+        if (element.startsWith('state=')) {
+            origURL = decodeURIComponent(element.substring(6, element.length));
+        }
+    })
+
+    let destination = origURL + '?c__code=' + authCode;
+
+    // TODO:
+    //      Redirection logic
+    document.getElementById('auth-code').innerHTML = authCode;
+    document.getElementById('state').innerHTML = origURL;
+
+    window.location.replace(destination);
+}
