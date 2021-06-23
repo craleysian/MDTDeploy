86457c1aff8c00c6e6ce5165a47baef191a18962
Cory Romdenne <cromdenne@aleysian.com>
2021-06-23 14:52:26 -0500

    Remove redirection for testing, add new state parameter

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ function redirect() {
     // Variables to pass back to the origination site (Metadata Tracker)
     let authCode = '';
     let origURL  = '';
+    let domain   = '';
 
     // Traverse the callback URL parameters
     url.forEach(element => {
@@ -16,14 +17,19 @@ function redirect() {
 
         // Extract the origination URL from the state parameter
         if (element.startsWith('state=')) {
-            origURL = decodeURIComponent(element.substring(6, element.length));
+            let state = decodeURIComponent(element.substring(6, element.length));
+            origURL = state.substring(0, state.indexOf('+'));
+            domain = state.substring(state.indexOf('+')+1, state.length);
         }
     })
 
-    let destination = origURL + '?c__code=' + authCode;
+    let destination = origURL + '?c__code=' + authCode + '&c__state=' + domain;
+    console.log(destination);
+    console.log(origURL);
+    console.log(domain);
 
     document.getElementById('auth-code').innerHTML = authCode;
     document.getElementById('state').innerHTML = origURL;
 
-    window.location.replace(destination);
+    //window.location.replace(destination);
 }
